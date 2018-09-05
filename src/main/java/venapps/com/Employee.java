9fa7e250ec680dee653106ef6b34c736bcd34da7
pc <pc@192.168.0.8>
2018-09-05 22:05:08 +0530

    i have removed unneccesary code

--- src/main/java/venapps/com/Employee.java
+++ src/main/java/venapps/com/Employee.java
@@ -2,27 +2,22 @@ package venapps.com;
 
 public class Employee {
 	public static void main(String[] args) {
-		int a=22;
-		a+=55;
-		a-=22;
+		int a = 22;
+		a += 55;
+		a -= 22;
 		google();
-		if(a>45)
+		if (a > 45)
 			System.out.println("good");
 		else
 			System.out.println("its ok");
-		
+
 		System.out.println("Heloo...!!!");
 	}
 
 	private static void google() {
 
-		int k=22;
+		int k = 22;
 		System.out.println("Awsome");
-		maps();
 	}
 
-	private static void maps() {
-
-		System.out.println("Well");
-	}
 }
